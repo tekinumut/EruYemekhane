76ae0f8b8349c12c980d03ccdf9cc8e1d7c24404
Umut Tekin <bm.umuttekin@outlook.com>
2021-06-10 18:32:51 +0300

    initial code release. project will develop from scratch

---
+++ app/src/main/java/com/tekinumut/eruyemekhane/App.kt
@@ -0,0 +1,11 @@
+package com.tekinumut.eruyemekhane
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
